57837e6db327e970c34e64d92dc61084c9faa0ae
john-clancy <clancasaurous@gmail.com>
2022-02-01 20:46:31 -0600

    README Function & API
    Finished up the readme function and the initialize app.  Added GitHub api

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // TODO: Include packages needed for this application (done)
 
-
 // External packages
 const inquirer = require('inquirer');
 const fs = require('fs');
@@ -97,10 +96,41 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+        return console.log(err);
+        }
+
+        console.log("Success! Your README.md file has been generated")
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+
+        // Prompt Inquirer questions
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Thank you for your responses! Fetching your GitHub data next...");
+    
+        // Call GitHub api for user info
+        const userInfo = await api.getUser(userResponses);
+        console.log("Your GitHub user info: ", userInfo);
+    
+        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
+        console.log("Generating your README next...")
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+    
+        // Write markdown to file
+        await writeFileAsync('ExampleREADME.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 // Function call to initialize app
 init();
