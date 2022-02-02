c76740b12da41045d4fd67ccebfecdd715097c25
john-clancy <clancasaurous@gmail.com>
2022-02-01 20:04:50 -0600

    Added questions
    Added the read-me file questions

---
+++ index.js
@@ -0,0 +1,106 @@
+// TODO: Include packages needed for this application (done)
+
+
+// External packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+
+// Internal packages
+const api = require('./utils/api');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
+// TODO: Create an array of questions for user input (done)
+const questions = [
+    {
+        type: 'input',
+        message: "What is your GitHub username? (NO @ needed)",
+        name: 'usernem',
+        validate: function (anwser) {
+            if (anwser.length <1){
+                return console.log("A valid GitHub username is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repo?",
+        name: 'repo',
+        default: 'readme-generator',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub repo is required for a badge.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title',
+        default: 'Project Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project title is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "List tools and utilities used to make this project",
+        name: 'Tools',
+        default: 'Project utilities',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "What is the general usage of this project",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
