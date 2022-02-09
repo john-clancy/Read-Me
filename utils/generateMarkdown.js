1ba42a1452c6abf7b7a7eccb124ddbe50be1e5bb
John Clancy <80930968+john-clancy@users.noreply.github.com>
2022-02-08 19:39:32 -0600

    Add files via upload
    Added final touches

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,122 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function generateMarkdown(userResponses, userInfo) {
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  // Generate Table of Contents conditionally based on userResponses
+  let draftToC = `## Table of Contents`;
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  if (userResponses.installation !== '') { draftToC += `
+  * [Installation](#installation)` };
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+  if (userResponses.usage !== '') { draftToC += `
+  * [Usage](#usage)` };
 
-`;
+
+  if (userResponses.tests !== '') { draftToC += `
+  * [Tests](#tests)` };
+
+
+  // Generate markdown for the top required portions of the README
+  let draftMarkdown = 
+  `# ${userResponses.title}
+  ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor)
+
+  
+  
+  ## Description 
+  
+  *The what, why, and how:* 
+  
+  ${userResponses.description}
+  `
+
+  // Add Table of Contents to markdown
+  draftMarkdown += draftToC;
+
+  // Add License section since License is required to Table of Contents
+  draftMarkdown += `
+  * [License](#license)`;
+  
+
+  // Optional Installation section
+  if (userResponses.installation !== '') {
+  
+  draftMarkdown +=
+  `
+  
+  ## Installation
+  
+  *Steps required to install project and how to get the development environment running:*
+  
+  ${userResponses.installation}`
+  };
+  
+
+  // Optional Usage section
+  if (userResponses.usage !== '') {
+  
+  draftMarkdown +=
+  
+  `
+  
+  ## Usage 
+  
+  *Instructions and examples for use:*
+  
+  ${userResponses.usage}`
+  };
+  
+  
+
+  // Optional Tests section
+  if (userResponses.tests !== '') {
+  
+  draftMarkdown +=
+  `
+  
+  ## Tests
+  
+  *Tests for application and how to run them:*
+  
+  ${userResponses.tests}`
+  };
+
+
+  // License section is required
+  draftMarkdown +=
+  `
+  
+  ## License
+  
+  ${userResponses.license}
+  `;
+
+
+  // Questions / About Developer section
+  let draftDev = 
+  `
+  ---
+  
+  ## Questions?
+  <img src="${userInfo.avatar_url}" alt="${userInfo.login}" width="40%" />
+  
+  For any questions, please contact me with the information below:
+
+  GitHub: [@${userInfo.login}](${userInfo.url})
+  `;
+
+  // If GitHub email is not null, add to Developer section
+  if (userInfo.email !== null) {
+  
+  draftDev +=
+  `
+  Email: ${userInfo.email}
+  `};
+
+  // Add developer section to markdown
+  draftMarkdown += draftDev;
+
+  // Return markdown
+  return draftMarkdown;
+  
 }
 
 module.exports = generateMarkdown;
